1a08583a8cd2dca77ebfd56bce9d147354f8891e
Wesley <wesley.bm124@gmail.com>
2022-01-22 22:50:35 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma_Intellij.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma_Intellij {
+    public static void main(String[] args) {
+        Gato gato = new Gato ();
+        System.out.println(gato);
+   /*     int a =15 ;
+        int b= 18;
+        System.out.println("Hello Word "+(a+b+a+b));*/
+    }
+}
+
